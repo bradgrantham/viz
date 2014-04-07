e891ac190b0476eabc4e28dd5c5df4e5211974df
Brad Grantham <grantham@plunk.org>
2014-04-07 09:37:14 -0700

    Add Apache license

--- library/vectormath.h
+++ library/vectormath.h
@@ -1,6 +1,19 @@
-/*
- * $Header: /home/grantham/cvsroot/projects/modules/singles/linmath.h,v 1.4 2007/02/01 05:57:01 grantham Exp $
- */
+//
+// Copyright 2013-2014, Bradley A. Grantham
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
 
 #ifndef __VECTORMATH_H__
 #define __VECTORMATH_H__
